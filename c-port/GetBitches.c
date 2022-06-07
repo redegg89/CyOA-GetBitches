9791e73629275ceb6aea3f1252a6036e91c810e4
redegg89 <78121676+redegg89@users.noreply.github.com>
2022-06-06 21:57:56 -0400

    Change indent size from 4 spaces to 2

--- c-port/GetBitches.c
+++ c-port/GetBitches.c
@@ -8,23 +8,23 @@
 
 void nam()
 {
-    char *name = malloc(MAX_NAME_SZ);
-    char nameselect[3];
-    while(1)
+  char *name = malloc(MAX_NAME_SZ);
+  char nameselect[3];
+  while(1)
+  {
+    printf("What is your name?\n");
+    fgets(name, MAX_NAME_SZ, stdin);
+    if ((strlen(name) > 0) && (name[strlen (name) - 1] == '\n'))
+    name[strlen (name) - 1] = '\0';
+    //scanf("%s", name);
+    printf("Your name is: %s? [Y/n] \n", name);
+    scanf("%s", nameselect);
+    //printf(nameselect);
+    if(strcmp(nameselect, "y") == 0 || strcmp(nameselect, "Y") == 0)
     {
-        printf("What is your name?\n");
-        fgets(name, MAX_NAME_SZ, stdin);
-        if ((strlen(name) > 0) && (name[strlen (name) - 1] == '\n'))
-        name[strlen (name) - 1] = '\0';
-        //scanf("%s", name);
-        printf("Your name is: %s? [Y/n] \n", name);
-        scanf("%s", nameselect);
-        //printf(nameselect);
-        if(strcmp(nameselect, "y") == 0 || strcmp(nameselect, "Y") == 0)
-        {
-            break;
-        }
+      break;
     }
+  }
 }
 
 void main() {
@@ -32,19 +32,19 @@ void main() {
   nam();
   while(1)
   {
-    input("Which route do you want to go?\n1. Slow and steady: More reliable, typically used for long-term relationships\n2. Get Bitches: Quick, great for one-night stands, almost guaranteed to fall off sometime in the next month\n");
-    switch(choice)
-    {
-        case 1:
-            printf("Have fun on your many dates to come!\n\n");
-            slow();
-            break;
-        case 2:
-            printf("Want to get bitches real quick, I see... Good luck.\n\n");
-            quick();
-            break;
-    }
-    break;
+  input("Which route do you want to go?\n1. Slow and steady: More reliable, typically used for long-term relationships\n2. Get Bitches: Quick, great for one-night stands, almost guaranteed to fall off sometime in the next month\n");
+  switch(choice)
+  {
+    case 1:
+      printf("Have fun on your many dates to come!\n\n");
+      slow();
+      break;
+    case 2:
+      printf("Want to get bitches real quick, I see... Good luck.\n\n");
+      quick();
+      break;
+  }
+  break;
   }
 } 
 
--- c-port/quick.c
+++ c-port/quick.c
@@ -7,49 +7,49 @@
 
 void quick()
 {
-    clear();
-    printf("You are in a bar. You notice 3 girls. Which would you like to bang?\n");
-    printf("1. A blonde with average-sized boobs. Given how she's drinking alone, she may have just gone through a breakup.\n2. A brunette with small boobs. She's hanging out with her friends, so she always has an excuse to leave.\n3. A redhead with larger breasts. She may be married, but you can't see a ring.\n");
-    input("Your choice: ");
-    switch(choice)
-    {
-        case 1:
-            blonde();
-            break;
-        case 2:
-            brunette();
-            break;
-        case 3:
-            redhead();
-            break;
-    }
+  clear();
+  printf("You are in a bar. You notice 3 girls. Which would you like to bang?\n");
+  printf("1. A blonde with average-sized boobs. Given how she's drinking alone, she may have just gone through a breakup.\n2. A brunette with small boobs. She's hanging out with her friends, so she always has an excuse to leave.\n3. A redhead with larger breasts. She may be married, but you can't see a ring.\n");
+  input("Your choice: ");
+  switch(choice)
+  {
+    case 1:
+      blonde();
+      break;
+    case 2:
+      brunette();
+      break;
+    case 3:
+      redhead();
+      break;
+  }
 }
 
 void blonde()
 {
-    printf("You have chosen the blonde. How would you like to approach her?\n1. \"Wanna fuck?\"\n2. \"You alright?\"\n3. \"Mind if I join you?\"\n");
-    input("Your choice: ");
-    switch(choice)
-    {
-        case 1:
-            printf("Sure.\n");
-            #ifdef _WIN32
-            Sleep(1000);
-            #else
-            sleep(1);
-            #endif
-            clear();
-            printf("Congratulations! You got laid!\n");
-            break;
-    }
+  printf("You have chosen the blonde. How would you like to approach her?\n1. \"Wanna fuck?\"\n2. \"You alright?\"\n3. \"Mind if I join you?\"\n");
+  input("Your choice: ");
+  switch(choice)
+  {
+    case 1:
+      printf("Sure.\n");
+      #ifdef _WIN32
+      Sleep(1000);
+      #else
+      sleep(1);
+      #endif
+      clear();
+      printf("Congratulations! You got laid!\n");
+      break;
+  }
 }
 
 void brunette()
 {
-    printf("brunette\n");
+  printf("brunette\n");
 }
 
 void redhead()
 {
-    printf("redhead\n");
+  printf("redhead\n");
 }
